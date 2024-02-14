a0c63295491c010f197aecbe61250214309a2ea0
JoshRov2 <joshr16@nycstudents.net>
2024-02-14 09:22:59 -0500

    update

---
+++ vite-project/src/views/StoreCard.vue
@@ -0,0 +1,69 @@
+<script setup>
+function onClick (name){
+  console.log(name)
+}
+const fishes = [
+    {
+        species: "Assorted African Cichlid(Pseudotropheus or Melanochromis spp.)",
+        price: 11.99,
+        img: "https://s7d2.scene7.com/is/image/PetSmart/4032869?$CLEARjpg$",
+        rating: 4.5,
+        type: "African Cichlid",
+    },
+    {
+        species: "Electric Yellow Labidochromis",
+        price: 10.99,
+        img: "https://s7d2.scene7.com/is/image/PetSmart/4031377?$sclp-prd-main_large$",
+        rating: 4.5,
+        type: "African Cichlid",
+    },
+    {
+        species: "Bumblebee Cichlid",
+        price: 6.00,
+        image: "https://s7d2.scene7.com/is/image/PetSmart/4031342?$sclp-prd-main_large$",
+        rating: 4.1,
+        type: "African Cichlids",
+    },
+    {
+        species: "Chinese Algae Eater",
+        price: 3.99,
+        img: "https://s7d2.scene7.com/is/image/PetSmart/4032006?$sclp-prd-main_large$",
+        rating: 3.8,
+        type: "Tank Cleaners",
+    },
+    {
+        species: "Otocinclus",
+        price: 2.49,
+        img: "https://s7d2.scene7.com/is/image/PetSmart/4032049?$sclp-prd-main_small$",
+        rating: 4.1,
+        type: "Tank Cleaner",
+    },
+    {
+        species: "Spotted Plecostomus",
+        price: 9.99,
+        img: "https://s7d2.scene7.com/is/image/PetSmart/4035311?$sclp-prd-main_small$",
+        rating: 4.0,
+        type: "Tank Cleaners",
+    },
+    {
+        species: "Angelfish",
+        price: 10.99,
+        img: "https://s7d2.scene7.com/is/image/PetSmart/4034664?$sclp-prd-main_small$",
+        rating: 4.0,
+        type: "South American Cichlids",
+    },
+]
+
+</script>
+<template>
+  <div id="appEl"></div>
+  <ul>
+    <li v-for="fish in fishes">
+    {{ fish.name }}
+    {{ fish.price }}
+    <img :src= "destination.img"/>
+    <button id="print" @click = "onClick(fish.name)">Select</button>
+    </li>
+  </ul>
+</template>
+
