5ee434a59b39ea46c9f6e2a13aec1f4356e285f9
JoshRov2 <joshr16@nycstudents.net>
2024-02-21 17:01:58 -0500

    update

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -1,15 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '../views/MainPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
+      component: MainPage
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/MainPage.vue')
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
